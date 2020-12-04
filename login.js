bc6651fbce05c495753043b6a0143d95dd27316d
glandalf <renaud@nucleotic.fr>
2020-12-04 13:33:24 +0100

    pose les bases d'un jeu type Kahoot en Vue

--- login.js
+++ login.js
@@ -1,10 +1,53 @@
+const questions = [
+    {
+        label: 'Quel est le meilleur framework front ?',
+        time: 5,
+        response1: 'Vue',
+        response2: 'Angular',
+        response3: 'React',
+        response4: 'JeanJean',
+        rightAnswer: 4,
+    },
+    {
+        label: 'Quel est le meilleur framework back ?',
+        time: 9,
+        response1: 'React',
+        response2: 'Angular',
+        response3: 'Vue',
+        response4: 'JeanJean',
+        rightAnswer: 4,
+    },
+]
+
 // On *instancie* un *objet* de la *classe* Vue
-const appLogin = new Vue({
-    el: '#app-login', // même format que les sélecteurs CSS
+const app = new Vue({
+    el: '#app', // même format que les sélecteurs CSS
     data: {
         identifiant: 'test',
         motDePasse: 'super secret',
-        estConnecte: false
+        estConnecte: true,
+        game: {
+            _intervalID: null,
+            title: '',
+            questions: [],  // Souci de watcher ?
+            pin: null,
+            status: 'idle', // lobby | pending | ended
+            currentQuestion: -1,
+            players: ['Bibbo', 'Bobbi'],
+            timer: null,
+        }
+    },
+    computed: {
+        questionsDuration() {
+            let total = 0;
+            this.game.questions.forEach(question => {
+                total += question.time;
+            })
+            return total;
+        },
+        question() {
+            return this.game.questions[this.game.currentQuestion];
+        }
     },
     methods: {
         onSubmit(ev) {
@@ -33,6 +76,43 @@ const appLogin = new Vue({
 
             }
         },
+
+
+        createGame() {
+            // Afficher un écran d'acceuil (lobby screen) avec création du code pin
+            this.game.pin = Math.floor(Math.random() * 1000000);
+            this.game.questions = questions;
+            this.game.players.push(this.identifiant);
+            this.game.status = 'lobby';
+        },
+        startGame() {
+            // Initialise des compteurs, affiche la première question
+            this.game.status = 'pending';
+            this.nextQuestion();
+        },
+        nextQuestion() {
+            // this.game.currentQuestion = this.game.currentQuestion + 1
+            if (this.game.currentQuestion < this.game.questions.length) {
+                this.game.currentQuestion++;
+                this.game.timer = this.question.time;
+                this.game._intervalID = setInterval(() => {
+                    if (this.game.timer > 0) {
+                        this.game.timer--;
+                    } 
+                    else {
+                        clearInterval(this.game._intervalID);
+                        this.nextQuestion();
+                    }
+                }, 1000);   
+            }
+            else {
+                this.endGame();
+            }
+        },
+        endGame() {
+            this.game.status = 'ended'
+        }
+
 
     }
     
